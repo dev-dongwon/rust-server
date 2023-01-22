b46029ff5f6ba1bdaf7acf262b07e386dfe2f754
dev-dongwon <dev.dongwonkim@gmail.com>
2023-01-22 13:01:23 +0900

    Feat: ADd lifetime on Request

--- src/http/request.rs
+++ src/http/request.rs
@@ -6,17 +6,18 @@ use std::error::Error;
 use std::fmt::{ Display, Formatter, Result as fmtResult, Debug };
 use std::str;
 
-pub struct Request {
-    path: String,
-    query_string: Option<String>, // none or Some<T>
+// 
+pub struct Request<'buf> {
+    path: &'buf str,
+    query_string: Option<&'buf str>, // none or Some<T>
     method: Method,
 }
 
 // GET /something?name=some HTTP/1.1\r\n..HEADERS..
-impl TryFrom<&[u8]> for Request {
+impl<'buf>  TryFrom<&'buf [u8]> for Request<'buf> {
     type Error = ParseError;
 
-    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+    fn try_from(buf: &'buf [u8]) -> Result<Request<'buf>, Self::Error> {
         let request = str::from_utf8(buf)?;
 
         // match get_next_word(request) {
@@ -35,15 +36,15 @@ impl TryFrom<&[u8]> for Request {
 
         let method: Method = method.parse()?;
 
-        let mut query_string = None;
+        let mut query_string: Option<&str> = None;
         // ?가 있으면 쿼리 스트링이 있음
         if let Some(i) = path.find('?') {
-            query_string = Some(path[i + 1..].to_string());
+            query_string = Some(&path[i + 1..]);
             path = &path[..i];
         }
 
         Ok(Self {
-            path: path.to_string(),
+            path,
             query_string,
             method
         })
