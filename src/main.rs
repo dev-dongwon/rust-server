e7c0a53cde0a46d30053e58170420f7954dc0316
dev-dongwon <dev.dongwonkim@gmail.com>
2023-02-10 11:12:23 +0900

    Feat: Add public path env

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use server::Server;
+use std::env;
 use website_handler::WebsiteHandler;
 
 mod server;
@@ -6,6 +7,8 @@ mod http;
 mod website_handler;
 
 fn main() {
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
     let server = Server::new(String::from("127.0.0.1:8080"));
-    server.run(WebsiteHandler);
+    server.run(WebsiteHandler::new(public_path));
 }
